9b37a1dc2143f05a160824d945354000f9516eda
XTIVI0658\mamde <mamde@xtivia.com>
2024-10-03 01:12:21 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-500 text-write min-h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold">Welcome!</h1>
+    </div>
+);
+}
---
+++ app/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="bg-blue-500 text-write min-h-screen flex items-center justify-center">
+            <h1 className="text-5xl font-bold">Welcome!</h1>
+        </div>
+    );
+}
