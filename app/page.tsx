659f57a17ee2029370011036f372fe9ea1e11412
XTIVI0658\mamde <mamde@xtivia.com>
2024-10-04 09:33:59 -0500

    Implement Contentful API intergration and blog fetching logic.
    -rewrite the  homepage to dynamically fetch content from contentful

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,117 @@
-import React from "react";
-import { GetStaticProps } from "next";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import * as contentful from 'contentful'
 
 interface BlogPost {
   slug: string;
-  pageName: string;
+  pageName: string
 }
 
 interface BlogProps {
   posts: BlogPost[];
 }
 
-const fetchContentfulData = async (): Promise<BlogPost[]> => {
-  const spaceId = "sioedqz4jhj9";
-  const environmentId = "master";
-  const accessToken = "Zp9D3ZBgF0W5VbwfCqJlf5w-cxa_n6sEhfJTVEGc8wc";
+const space = process.env.CONTENTFUL_SPACE_ID
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN 
 
-  const query = `
-  {
-    pageCollection {
-      items {
-      pageName
-      slug
+// if(!space || !accessToken){
+//   throw new Error
+// }
+
+const BlogPage = () => {
+  const [posts, setPosts] = useState<BlogPost[]>([]);
+  
+  const space = process.env.CONTENTFUL_SPACE_ID || 'sioedqz4jhj9'
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN || "U72dx5dJS7PUGgQPlfyA5jHfoUcPvMC9d294kYl54TE"
+
+  if(!space || !accessToken){
+    throw new Error("missing contentful cofiguration");
+  }
+
+  const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: space,
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: accessToken,
+  })
+
+
+  async function fetchEntries(contentType : string, slug : string) {
+    if(accessToken){
+      const entries = await client.getEntries({
+        content_type: 'page',
+        "fields.slug": slug,
+        include: 5    
+      })
+      if (entries.items) {
+        console.log("entires", entries.items)
+        return entries.items
+      }
+      console.log(`Error getting Entries for ${contentType}.`)
     }
+    console.log(`Access Token is undefined`);
+  }
+
+  async function fetchPosts() {
+    const url = "https://jsonplaceholder.typicode.com/todos/";
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+         throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("response", response);
+      setPosts(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
     }
   }
-  `;
 
-  // const response = await fetch(
-  //   'https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environmentId}',
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${accessToken}`,
-  //     },
-  //     body: JSON.stringify({ query }),
-  //   }
-  // );
+  useEffect(() => {
+    fetchEntries("ContentType", "slug");
+  }, []);
 
-  // const url = `https://graphql.contentful.com/content/v1/spaces/sioedqz4jhj9/explore?access_token=Zp9D3ZBgF0W5VbwfCqJlf5w-cxa_n6sEhfJTVEGc8wc`;
-  const url = `https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environmentId}?access_token=${accessToken}&query=${query}`;
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-  return data?.data?.pageCollection.items || [];
-};
-
-const BlogPage = async () => {
-  const posts = await fetchContentfulData();
   return (
     <div>
       <h1>Blog posts</h1>
       {posts.map((post) => (
-        <div key={post.slug}></div>
-      ))}
+      <div key={post.slug}>{post.pageName}</div>
+     ))}
     </div>
   );
 };
 
 export default BlogPage;
+// 'use client'
+
+// import { useState, useEffect } from 'react'
+
+// function Posts() {
+//   const [posts, setPosts] = useState(null)
+
+//   useEffect(() => {
+//     async function fetchPosts() {
+//       const url = 'https://api.vercel.app/blog';
+//       const res = await fetch(url)
+//       const data = await res.json()
+//       setPosts(data)
+//       console.log(data);
+//     }
+//     fetchPosts()
+//   }, [])
+
+//   if (!posts) return <div>Loading...</div>
+
+//   return (
+//     <ul>
+//       {/* {posts.map((post) => (
+//         <li key={post.id}>{post.title}</li>
+//       ))} */}
+//     </ul>
+//   )
+// }
+
+// export default Posts;
